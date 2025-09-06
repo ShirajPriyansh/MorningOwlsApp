f01782f2914be39cde7f5c77e20b5465d0ce53eb
Priyansh Kumar <shirajpriyansh22@gmail.com>
2025-09-06 17:21:05 +0000

    fix issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,7 @@
 
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
-
-const OwlIcon = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg 
-        xmlns="http://www.w3.org/2000/svg" 
-        viewBox="0 0 24 24" 
-        fill="none" 
-        stroke="currentColor" 
-        strokeWidth="2" 
-        strokeLinecap="round" 
-        strokeLinejoin="round" 
-        {...props}
-    >
-        <path d="M14.5 13c-2.2 0-4-1.8-4-4s1.8-4 4-4 4 1.8 4 4-1.8 4-4 4z"/>
-        <path d="M9.5 13c-2.2 0-4-1.8-4-4s1.8-4 4-4 4 1.8 4 4-1.8 4-4 4z"/>
-        <path d="M12 22C6.5 22 2 17.5 2 12S6.5 2 12 2s10 4.5 10 10-4.5 10-10 10z"/>
-        <path d="M12 15c-3 0-5.5 2.5-5.5 5.5"/>
-    </svg>
-);
-
+import { OwlIcon } from '@/components/logo';
 
 export default function Home() {
   return (
